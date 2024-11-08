70d7c0a9471d198c2870d66cec87b0b6522213fc
Bryan González <bryangonzalezfallas@gmail.com>
2024-11-07 22:34:38 -0600

    bgf13.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/bgf13.py
@@ -0,0 +1,29 @@
+# https://www.python.org/ 
+
+# Con la almuadilla se crean comentarios de una linea
+
+""" 
+con triple comillas
+se puede crear un comentario
+con varias lineas
+"""
+
+'''
+Tambien las comillas 
+simples crean comentarios
+de varias lineas
+'''
+
+my_variable = " 55 "
+my_variable = " 58 "
+# Las variables pueden cambiar
+
+MY_CONSTANT = " CONSTANTE "
+# Las constantes dan a entender que no se cambia durante el uso del programa
+
+my_int = 25
+my_float = 3.14
+my_bool = False
+my_string = "cadenas de textos"
+print("hello, phyton")
+
